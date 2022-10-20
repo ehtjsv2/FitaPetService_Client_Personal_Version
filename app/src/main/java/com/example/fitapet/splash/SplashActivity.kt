19cf9a5782ae5df5b931db85ca332bc4d0166054
ehtjsv2 <ehtjsv2@naver.com>
2022-10-20 21:13:38 +0900

    login ui/ux, no api, no DTO

--- app/src/main/java/com/example/fitapet/splash/SplashActivity.kt
+++ app/src/main/java/com/example/fitapet/splash/SplashActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
 import com.example.fitapet.MainActivity
 import com.example.fitapet.R
+import com.example.fitapet.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -20,7 +21,8 @@ class SplashActivity : AppCompatActivity() {
         //      postDelayed()를 통해 일정 시간(딜레이 시간)동안 쓰레드 작업을 멈춘다.
         //      {딜레이 이후 동작}을 통해 딜레이 시간 이후, 동작을 정의해준다.
         Handler().postDelayed(splashDuration){
-            val intent = Intent(this, MainActivity::class.java)
+            //val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
