b2109b949903edbe36357930a0d590c072284a1e
HeewonP825 <7hwon@naver.com>
2022-10-30 20:14:20 +0900

    backup

--- app/src/main/java/com/example/fitapet/login/LoginActivity.kt
+++ app/src/main/java/com/example/fitapet/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.fitapet.MainActivity
 import com.example.fitapet.databinding.ActivityLoginBinding
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
@@ -26,7 +27,7 @@ class LoginActivity : AppCompatActivity() {
                         Log.e(ContentValues.TAG, "로그인 실패", error)
                     } else if (token != null) {
                         Log.i(ContentValues.TAG, "로그인 성공 ${token.accessToken}")
-                        val intent = Intent(this, NaviActivity::class.java)
+                        val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                         finish()
                     }
