125465d088b0e708b44600059fdde2465ecd8d20
HeewonP825 <7hwon@naver.com>
2022-11-06 07:32:37 +0900

    home fragment xml

--- app/src/main/java/com/example/fitapet/navfragment/MypageFragment.kt
+++ app/src/main/java/com/example/fitapet/navfragment/MypageFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.fitapet.R
 
-class FriendFragment : Fragment() {
+class MypageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
 }
