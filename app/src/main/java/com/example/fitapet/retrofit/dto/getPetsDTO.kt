ff8a7a1685c5d7edd16894b3ba64a67104a54a67
J-MU <alsdnrdl001@gmail.com>
2022-11-07 01:50:23 +0900

    api 2개 bind

--- app/src/main/java/com/example/fitapet/retrofit/dto/getPetsDTO.kt
+++ app/src/main/java/com/example/fitapet/retrofit/dto/getPetsDTO.kt
@@ -4,13 +4,16 @@ data class getPetsDTO(
     val isSuccess:String,
     val code:Int,
     val message:String,
-    val result:Pets
+    val result:List<Pets>
+
+
 )
 
 data class Pets(
     val petId:Int,
     val petName:String,
     val petType:String,
+    val petSpecies:String,
     val petBirth:String,
     val petSize:String,
     val petSex:String,
